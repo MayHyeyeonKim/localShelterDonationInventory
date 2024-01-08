888e1413311b5e2fed9d7f3203b91acc8add592b
MayHyeyeonKim <hkim7963@gmail.com>
2024-01-08 00:27:06 -0800

    connecting frontend and backend

--- server/app.js
+++ server/app.js
@@ -1,23 +1,30 @@
 import express from "express";
 import cors from 'cors';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 import mainRoutes from './routes/mainRouters.js';
-import donationRouters from './routes/donationRouters.js';
+import donationRoutes from './routes/donationRouters.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 const port = 3000;
 
 app.use(cors());
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/main', mainRoutes);
-app.use('/donations', donationRouters);
+app.use('/donations', donationRoutes);
 
-app.get('/', (req,res) => {
-    res.send('Welcome to Mays Inventory!');
+app.use(express.static('public'));
+app.use(express.static(dirname(__dirname, 'client')));
+
+app.get('*', (req, res) => {
+    res.sendFile('./client/index.html', { root: dirname(__dirname, 'client') });
 });
 
-app.listen(port,() => {
-    console.log(`server is running at http://localhost:${port}`);
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
 });
