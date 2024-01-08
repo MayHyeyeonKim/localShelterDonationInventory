e11cd733c83ba4cc463f194b77db1ea34c43e2eb
MayHyeyeonKim <hkim7963@gmail.com>
2024-01-08 00:00:03 -0800

    added : routers

--- server/app.js
+++ server/app.js
@@ -1,7 +1,18 @@
 import Express from "express";
+import cors from 'cors';
+import mainRoutes from './routes/mainRouters';
+import donationRouters from './routes/donationRouters';
 
 const app = Express();
 const port = 3000;
+
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/main', mainRoutes);
+app.use('/donations', donationRouters);
 
 app.get('/', (req,res) => {
     res.send('Welcome to Mays Inventory!');
