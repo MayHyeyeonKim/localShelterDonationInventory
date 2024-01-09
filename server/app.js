f5ebf843bed9c5e3baa079f636dc3a7c3576516e
MayHyeyeonKim <hkim7963@gmail.com>
2024-01-08 23:44:21 -0800

    connected mongodb

--- server/app.js
+++ server/app.js
@@ -1,51 +1,48 @@
-import express from "express";
+import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
-import dotenv from 'dotenv';
-
 import mainRoutes from './routes/mainRouters.js';
 import donationRoutes from './routes/donationRouters.js';
+import dotenv from 'dotenv';
 
 dotenv.config();
+// console.log('Loaded environment variables:', process.env);
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.error(err));
+
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose
-    .connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    .then(() => {
-        console.log("MongoDB Connected...");
+app.use('/main', mainRoutes);
+app.use('/donations', donationRoutes);
 
-        app.use('/main', mainRoutes);
-        app.use('/donations', donationRoutes);
+app.use(express.static('public'));
+app.use(express.static(join(__dirname, 'client')));
 
-        app.use(express.static('public'));
-        app.use(express.static(join(__dirname, 'client')));
+app.get('/donation.html', (req, res) => {
+    res.sendFile(join(__dirname, '../client/donation.html'));
+});
 
-        app.get('/donation.html', (req, res) => {
-            res.sendFile(join(__dirname, '../client/donation.html'));
-        });
+app.get('*', (req, res) => {
+    res.sendFile(join(__dirname, '../client/index.html'));
+});
 
-        app.get('*', (req, res) => {
-            res.sendFile(join(__dirname, '../client/index.html'));
-        });
-
-        app.listen(port, () => {
-            console.log(`Server is running at http://localhost:${port}`);
-        });
-    })
-    .catch((err) => {
-        console.error('Error connecting to MongoDB:', err);
-    });
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
