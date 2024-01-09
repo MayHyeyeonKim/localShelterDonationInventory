391505e3450c7ee490542472420449de59924397
MayHyeyeonKim <hkim7963@gmail.com>
2024-01-08 23:14:53 -0800

    test uploding .env

--- server/app.js
+++ server/app.js
@@ -1,9 +1,14 @@
 import express from "express";
 import cors from 'cors';
+import mongoose from 'mongoose';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
+import dotenv from 'dotenv';
+
 import mainRoutes from './routes/mainRouters.js';
 import donationRoutes from './routes/donationRouters.js';
+
+dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -15,20 +20,32 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/main', mainRoutes);
-app.use('/donations', donationRoutes);
+mongoose
+    .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("MongoDB Connected...");
 
-app.use(express.static('public'));
-app.use(express.static(join(__dirname, 'client')));
+        app.use('/main', mainRoutes);
+        app.use('/donations', donationRoutes);
 
-app.get('/donation.html', (req, res) => {
-    res.sendFile(join(__dirname, '../client/donation.html'));
-});
+        app.use(express.static('public'));
+        app.use(express.static(join(__dirname, 'client')));
 
-app.get('*', (req, res) => {
-    res.sendFile(join(__dirname, '../client/index.html'));
-});
+        app.get('/donation.html', (req, res) => {
+            res.sendFile(join(__dirname, '../client/donation.html'));
+        });
 
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
-});
+        app.get('*', (req, res) => {
+            res.sendFile(join(__dirname, '../client/index.html'));
+        });
+
+        app.listen(port, () => {
+            console.log(`Server is running at http://localhost:${port}`);
+        });
+    })
+    .catch((err) => {
+        console.error('Error connecting to MongoDB:', err);
+    });
